08e8582fbb57c4fcd5f9228c92e5376dc4b0acb1
ArjanSchouten <arjan.schouten@solureal.com>
2022-09-06 15:05:09 +0200

    fix: remove admin-container script.

--- admin-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "CAUTION. You have privileged access to the nodes filesystem. Be CAREFUL!!!!"
-echo "Gaining access to node: $1..."
-trap "kubectl delete pod busybox --now" EXIT
-cat "pod.yaml" | sed "s/nodeName: .*/nodeName: $1/" | kubectl apply -f -
-kubectl wait --for=condition=Ready pod/busybox
-kubectl exec -it busybox -- sh
